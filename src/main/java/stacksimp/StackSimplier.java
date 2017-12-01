7a7a5abe6d304f74eb03cb2ba9fd2383cd94b4a6
madvirus <madvirus@madvirus.net>
2017-12-01 10:11:06 +0900

    add includePatterns

--- src/main/java/stacksimp/StackSimplier.java
+++ src/main/java/stacksimp/StackSimplier.java
@@ -1,62 +1,92 @@
 package stacksimp;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StackSimplier {
-	private static Pattern DEFAULT_PATTERN = Pattern.compile("^sun|^java|^com.sun");
-	
-	private Pattern excludeClassPattern;
-	private boolean useDefaultExcludeClassPattern = true;
+    private boolean useDefaultExcludeClassPattern = true;
+    private static ClassNamePatternMatcher DEFAULT_EXCLUDE_MATCHER = new ClassNamePatternMatcher("^sun", "^java", "^com.sun");
+    private ClassNamePatternMatcher excludeMatcher;
+    private ClassNamePatternMatcher includeMatcher;
 
-	public String simplify(Throwable exception) {
-		StringBuilder buff = new StringBuilder();
-		buff.append(exception.getClass().getName()).append(": ").append(exception.getMessage()).append("\n");
-		StackTraceElement[] traces = exception.getStackTrace();
-		filterAndAppend(buff, traces);
-		
-		if (exception.getCause() != null) {
-			Throwable cause = exception.getCause();
-			buff.append("Caused by: ").append(cause.getClass().getName()).append(": ").append(cause.getMessage()).append("\n");
-			filterAndAppend(buff, cause.getStackTrace());
-		}
-		return buff.toString();
-	}
+    public String simplify(Throwable exception) {
+        StringBuilder buff = new StringBuilder();
+        buff.append(exception.getClass().getName()).append(": ").append(exception.getMessage()).append(System.lineSeparator());
+        StackTraceElement[] traces = exception.getStackTrace();
+        filterAndAppend(buff, traces);
 
-	private void filterAndAppend(StringBuilder buff, StackTraceElement[] traces) {
-		boolean firstIncluded = false;
-		for (StackTraceElement stackTrace : traces) {
-			if (!firstIncluded) {
-				firstIncluded = true;
-				buff.append("\tat ").append(stackTrace).append("\n");
-			} else if (macthIncludeTrace(stackTrace)) {
-				buff.append("\tat ").append(stackTrace).append("\n");
-			}
-		}
-	}
+        if (exception.getCause() != null) {
+            Throwable cause = exception.getCause();
+            buff.append("Caused by: ").append(cause.getClass().getName()).append(": ").append(cause.getMessage()).append(System.lineSeparator());
+            filterAndAppend(buff, cause.getStackTrace());
+        }
+        return buff.toString();
+    }
 
-	private boolean macthIncludeTrace(StackTraceElement stackTrace) {
-		if (matchExcludeClass(stackTrace.getClassName())) {
-			return false;
-		}
-		return true;
-	}
+    private void filterAndAppend(StringBuilder buff, StackTraceElement[] traces) {
+        boolean firstIncluded = false;
+        for (StackTraceElement stackTrace : traces) {
+            if (!firstIncluded) {
+                firstIncluded = true;
+                buff.append("\tat ").append(stackTrace).append("\n");
+            } else if (macthIncludeTrace(stackTrace)) {
+                buff.append("\tat ").append(stackTrace).append("\n");
+            }
+        }
+    }
 
-	private boolean matchExcludeClass(String className) {
-		if (useDefaultExcludeClassPattern && DEFAULT_PATTERN.matcher(className).find()) return true;
-		if (excludeClassPattern != null && excludeClassPattern.matcher(className).find()) return true;
-		return false;
-	}
+    private boolean macthIncludeTrace(StackTraceElement stackTrace) {
+        if (matchIncludeClass(stackTrace.getClassName())) {
+            return true;
+        }
+        if (matchExcludeClass(stackTrace.getClassName())) {
+            return false;
+        }
+        return true;
+    }
 
-	public void setExcludeClassPatterns(String[] patterns) {
-		String regex = Arrays.stream(patterns)
-				.map(pat -> pat.replace(".", "\\."))
-				.collect(Collectors.joining("|"));
-		excludeClassPattern = Pattern.compile(regex);
-	}
+    private boolean matchIncludeClass(String className) {
+        return includeMatcher != null && includeMatcher.match(className);
+    }
 
-	public void setUseDefaultExcludeClassPattern(boolean useDefaultExcludeClassPattern) {
-		this.useDefaultExcludeClassPattern = useDefaultExcludeClassPattern;
-	}
+    private boolean matchExcludeClass(String className) {
+        if (useDefaultExcludeClassPattern && DEFAULT_EXCLUDE_MATCHER.match(className)) return true;
+        if (excludeMatcher != null && excludeMatcher.match(className)) return true;
+        return false;
+    }
+
+    public void setUseDefaultExcludeClassPattern(boolean useDefaultExcludeClassPattern) {
+        this.useDefaultExcludeClassPattern = useDefaultExcludeClassPattern;
+    }
+
+    public void setExcludeClassPatterns(String... patterns) {
+        excludeMatcher = new ClassNamePatternMatcher(patterns);
+    }
+
+    public boolean isUseDefaultExcludeClassPattern() {
+        return useDefaultExcludeClassPattern;
+    }
+
+    public void setIncludeClassPatterns(String... patterns) {
+        includeMatcher = new ClassNamePatternMatcher(patterns);
+    }
+
+    private static class ClassNamePatternMatcher {
+        private List<String> patterns;
+        private Pattern pattern;
+
+        public ClassNamePatternMatcher(String... patterns) {
+            this.patterns = Arrays.asList(patterns);
+            String regex = Arrays.stream(patterns)
+                    .map(pat -> pat.replace(".", "\\."))
+                    .collect(Collectors.joining("|"));
+            pattern = Pattern.compile(regex);
+        }
+
+        boolean match(String className) {
+            return pattern.matcher(className).find();
+        }
+    }
 }
--- src/test/java/stacksimp/StackSimplierTest.java
+++ src/test/java/stacksimp/StackSimplierTest.java
@@ -2,33 +2,73 @@ package stacksimp;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StackSimplierTest {
-    private final String[] patterns = {"org.junit", "com.intellij"};
 
     @Test
-    public void simplify() throws Exception {
+    public void when_Use_DefaultExclude_And_AdditonalExclude() throws Exception {
+        String[] patterns = {"org.junit", "com.intellij"};
         StackSimplier simplier = new StackSimplier();
         simplier.setExcludeClassPatterns(patterns);
+        assertSimplifier(simplier,
+                new String[]{"org.junit", "com.intellij", "^sun", "^java", "^com.sun"},
+                new String[0]);
+    }
+
+    @Test
+    public void when_DoNot_UseDefaultExclude_And_Use_AdditronalExclude() throws Exception {
+        String[] patterns = {"org.junit", "com.intellij"};
+        StackSimplier simplier = new StackSimplier();
+        simplier.setUseDefaultExcludeClassPattern(false);
+        simplier.setExcludeClassPatterns(patterns);
+        assertSimplifier(simplier,
+                new String[]{"org.junit", "com.intellij"},
+                new String[]{"^sun", "^java"});
+    }
+
+    @Test
+    public void when_Use_AdditronalExclude_And_Use_Include() throws Exception {
+        String[] patterns = {"org", "com.intellij"};
+        StackSimplier simplier = new StackSimplier();
+        simplier.setUseDefaultExcludeClassPattern(false);
+        simplier.setExcludeClassPatterns(patterns);
+        simplier.setIncludeClassPatterns("org.junit");
+        assertSimplifier(simplier,
+                new String[]{"org", "com.intellij"},
+                new String[]{"org.junit"});
+    }
+
+    private void assertSimplifier(StackSimplier simplier, String[] excludePatterns, String[] includePatterns) {
         try {
             URL url = new URL("http://bad.server.xx");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.getResponseCode();
-        } catch(Exception ex) {
+        } catch (Exception ex) {
+            StringWriter sw = new StringWriter();
+            ex.printStackTrace(new PrintWriter(sw));
             String simplified = simplier.simplify(ex);
-            System.out.println(simplified);
-            assertSimplifiedStackTrace(simplified);
+            assertSimplifiedStackTrace(sw.toString(), simplified, excludePatterns, includePatterns);
         }
     }
 
-    private void assertSimplifiedStackTrace(String simplified) {
+    private void assertSimplifiedStackTrace(String full, String simplified, String[] excludePatterns, String[] includePatterns) {
+        Set<String> mustHaveLines = Arrays.stream(lines(full))
+                .filter(s -> s.startsWith("\tat "))
+                .filter(s -> matchPatterns(s, includePatterns))
+                .collect(Collectors.toSet());
+
         String[] traceLines = lines(simplified);
         int stackTraceLineNum = -1;
-
         for (String line : traceLines) {
             if (!line.startsWith("\tat")) {
                 stackTraceLineNum = 0;
@@ -36,8 +76,16 @@ public class StackSimplierTest {
                 stackTraceLineNum++;
             }
             if (stackTraceLineNum > 1) {
-                assertStackTractContainsNoExcludeClassPattern(line);
+                boolean excludeMatch = matchPatterns(line, excludePatterns);
+                boolean includeMatch = matchPatterns(line, includePatterns);
+                assertFalse(excludeMatch && !includeMatch,
+                        line + " do not have to contains exlucePatterns");
             }
+        }
+
+        for (String mustHaveLine : mustHaveLines) {
+            assertTrue(Arrays.stream(traceLines).anyMatch(line -> line.trim().equals(mustHaveLine.trim())),
+                    String.format("simplified result must contains %s", mustHaveLine));
         }
     }
 
@@ -45,11 +93,14 @@ public class StackSimplierTest {
         return str.split("\n");
     }
 
-    private void assertStackTractContainsNoExcludeClassPattern(String line) {
+    private boolean matchPatterns(String line, String[] patterns) {
         for (String pattern : patterns) {
             if (pattern.startsWith("^")) {
-                assertFalse(line.startsWith("\tat " + pattern.substring(1)),line + " dont't startWith " + pattern);
+                if (line.startsWith("\tat " + pattern.substring(1))) return true;
+            } else {
+                if (line.contains(pattern)) return true;
             }
         }
+        return false;
     }
 }
